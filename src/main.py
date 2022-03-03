7f7cea9de713fdf00bf90bf42b78e584abff8990
yifan <yifan.hou@inf.ethz.ch>
2022-03-03 23:22:37 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
                         help="Adam epsilon")
     parser.add_argument("--warmup_steps", type=int, default=1e4,
                         help="number of training epochs.")
-    parser.add_argument("--entity_epoch", type=int, default=2,
+    parser.add_argument("--entity_epoch", type=int, default=5,
                         help="number of training epochs.")
-    parser.add_argument("--triple_epoch", type=int, default=5,
+    parser.add_argument("--triple_epoch", type=int, default=10,
                         help="number of training epochs.")
     parser.add_argument("--patience", type=int, default=10,
                         help="used for early stop")
-    parser.add_argument("--batch_num", type=int, default=128,
+    parser.add_argument("--batch_num", type=int, default=32,
                         help="number of triple samples per 1 batch")
     parser.add_argument("--neg_num", type=int, default=8,
                         help="number of negative samples")
